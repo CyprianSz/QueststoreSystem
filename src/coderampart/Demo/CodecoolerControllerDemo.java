b8483afde952769a11d3bde4e03d888df2550adc
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-09-07 15:48:08 +0200

    Implement codecooler controller

--- src/coderampart/Demo/CodecoolerControllerDemo.java
+++ src/coderampart/Demo/CodecoolerControllerDemo.java
@@ -1,6 +1,8 @@
-package coderampart.Demo;
+package coderampart.demo;
 
 import coderampart.model.Codecooler;
+import coderampart.services.Bootable;
+import coderampart.view.View;
 
 public class CodecoolerControllerDemo implements Bootable {
 
@@ -23,5 +25,31 @@ public class CodecoolerControllerDemo implements Bootable {
             case DISPLAY_LEVEL: displayLevel();
                     break;
         }
+    }
+
+    public static void displayWallet(){
+        System.out.println("Your wallet:");
+        System.out.println("Balance: 500 coolcoins");
+        System.out.println("Bought items: Private mentoring");
+    }
+
+    public static void buyArtifact(){
+        System.out.println("Current balance: 500cc");
+        System.out.println("Choose an item:");
+        System.out.println("\n1. Combat training, 50cc"
+                         + "\n2. Sanctuary, 300cc"
+                         + "\n3. Time Travel, 500cc");
+
+        Integer artifactChoice = View.getUserChoice();
+        if (artifactChoice >= 0) {
+            System.out.println("Item bought!");
+        }
+    }
+
+    public static void buyWithGroup(){
+    }
+
+    public static void displayLevel(){
+        System.out.println("");
     }
 }
