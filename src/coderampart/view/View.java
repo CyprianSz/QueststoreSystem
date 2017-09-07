4d689eae4f87a40932d0af023b97a91b9af2d5ca
monzag <zaganiacz.m@gmail.com>
2017-09-07 17:41:55 +0200

    Add displayCodecoolerMenu

--- src/coderampart/view/View.java
+++ src/coderampart/view/View.java
@@ -9,8 +9,16 @@ public class View {
 
     public static void displayMentorMenu() {
         ArrayList<String> options = new ArrayList<>(Arrays.asList("Create new Codecooler", "Add quest", "Update quest",
-                                                             "Set quest category", "Mark quest", "Add artifact", "Update artifact", 
-                                                             "Set artifact type", "Mark artifact", "Display wallet details"));
+                                                                  "Set quest category", "Mark quest", "Add artifact", "Update artifact", 
+                                                                  "Set artifact type", "Mark artifact", "Display wallet details"));
+
+        displayOptions(options);
+        System.out.println("\n0. Exit");
+    }
+
+    public static void displayCodecoolerMenu() {
+        ArrayList<String> options = new ArrayList<>(Arrays.asList("Display wallet", "Buy artifact", "Buy artifact with group",
+                                                                  "Display level"));
 
         displayOptions(options);
         System.out.println("\n0. Exit");
