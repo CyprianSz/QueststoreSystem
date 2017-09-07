c81e2acceee8d14255ed51bdfcd451fcf4ebcf7e
monzag <zaganiacz.m@gmail.com>
2017-09-07 16:53:35 +0200

    Add displayMentorMenu

--- src/coderampart/view/View.java
+++ src/coderampart/view/View.java
@@ -2,4 +2,13 @@ package coderampart.view;
 
 public class View {
 
+    public static void displayMentorMenu() {
+        ArrayList<String> options = new ArrayList<>(Arrays.asList("Create new Codecooler", "Add quest", "Update quest",
+                                                             "Set quest category", "Mark quest", "Add artifact", "Update artifact", 
+                                                             "Set artifact type", "Mark artifact", "Display wallet details"));
+
+        displayOptions(options);
+        System.out.println("\n0. Exit");
+    }
+
 }
