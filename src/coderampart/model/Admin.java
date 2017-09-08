56dba18bd5b15026baa3783b1d292792c77c95ad
CyprianS <szwajka.cyprian@gmail.com>
2017-09-08 13:31:41 +0200

    Add 'super()' to construcotor in 'Admin' and 'Mentor' model

--- src/coderampart/model/Admin.java
+++ src/coderampart/model/Admin.java
@@ -4,22 +4,13 @@ import java.time.LocalDate;
 
 public class Admin extends AbstractUser {
 
+    // TODO: remove after testing
     public Admin() {
-        this.ID = null;
-        this.name = null;
-        this.surname = null;
-        this.email = null;
-        this.password = null;
-        this.dateOfBirth = null;
+        super();
     }
 
     public Admin(String name, String surname, String email, LocalDate dateOfBirth) {
-        this.ID = null; // ID generatoro here
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = null; // some method to create password here
-        this.dateOfBirth = dateOfBirth;
+        super(name, surname, email, dateOfBirth);
     }
 
 }
--- src/coderampart/model/Mentor.java
+++ src/coderampart/model/Mentor.java
@@ -6,23 +6,14 @@ public class Mentor extends AbstractUser {
 
     private Group group;
 
+    // TODO: remove after testing
     public Mentor() {
-        this.ID = null;
-        this.name = null;
-        this.surname = null;
-        this.email = null;
-        this.password = null;
-        this.dateOfBirth = null;
+        super();
         this.group = null;
     }
 
     public Mentor(String name, String surname, String email, LocalDate dateOfBirth) {
-        this.ID = null; // ID generatoro here
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = null; // some method to create password here
-        this.dateOfBirth = dateOfBirth;
+        super(name, surname, email, dateOfBirth);
     }
 
     public Group getGroup() { return this.group; }
