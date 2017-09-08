c0ba6ff940eb02b557d9b2146fdd9c12b9ff41a0
CyprianS <szwajka.cyprian@gmail.com>
2017-09-08 13:27:18 +0200

    Create constructor in 'AbstarctUser'

--- src/coderampart/model/AbstractUser.java
+++ src/coderampart/model/AbstractUser.java
@@ -11,6 +11,25 @@ public abstract class AbstractUser {
     protected String password;
     protected LocalDate dateOfBirth;
 
+    // TODO: remove after testing
+    public AbstractUser() {
+        this.ID = null;
+        this.name = null;
+        this.surname = null;
+        this.email = null;
+        this.password = null;
+        this.dateOfBirth = null;
+    }
+
+    public AbstractUser(String name, String surname, String email, LocalDate dateOfBirth) {
+        this.ID = null; // TODO: ID generatoro here
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = null; // TODO: some method to create password here
+        this.dateOfBirth = dateOfBirth;
+    }
+
     public String getID() { return this.ID; }
     public String getName() { return this.name; }
     public String getSurname() { return this.surname; }
