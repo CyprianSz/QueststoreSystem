a95472415afd4a9edd788b99623b884412a2ce72
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 17:51:24 +0200

    Create migration file for teams data insertion

---
+++ src/main/resources/db/migration/V14__Insert_teams_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO codecoolers
+(id, group_id, name)
+VALUES
+  ('fc39737e-b364-4168-bb51-33acaf47e35e',
+   '85bdd453-6f22-4d75-a2ca-95cfb1326e74',
+   'Team unicorns'),
+
+  ('c37ffeb3-6a9e-4d94-8b8e-fbb2c08b73d9',
+   '58f9dfa7-ecc1-4da9-8946-12872b6366db',
+   'Noob team'),
+
+  ('c37ffeb3-6a9e-4d94-8b8e-fbb2c08b73d9',
+   '58f9dfa7-ecc1-4da9-8946-12872b6366db',
+   'Code ramparts')
+;
