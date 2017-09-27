a5a3f93c4819d2315167ca0f26e04333fb777b64
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 18:08:24 +0200

    Create migration for quests data insertion

---
+++ src/main/resources/db/migration/V18__Insert_quests_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO codecoolers
+(id, name, description, reward)
+VALUES
+  ('f7d3964a-2e3e-4722-99be-78622db6fb97',
+   'Exploring a dungeon',
+   'Finishing a Teamwork week',
+   '100'),
+
+  ('5dd16490-2c58-4200-859c-d13b5d12b4b4',
+   'Solving the magic puzzle',
+   'Finishing an SI assignment',
+   '100'),
+
+  ('6feb16b0-b6f6-4d3f-8818-c96f1d8a3484',
+   'Slaying a dragon',
+   'Passing a Checkpoint in the first attempt',
+   '500')
+;
