89e42abd19bee7944ddf4a7a128a63cbd49e1b10
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 18:21:38 +0200

    Change wrong spelling

--- src/main/resources/db/migration/V14__Insert_teams_data.sql
+++ src/main/resources/db/migration/V14__Insert_teams_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO codecoolers
+INSERT INTO teams
 (id, group_id, name)
 VALUES
   ('fc39737e-b364-4168-bb51-33acaf47e35e',
@@ -9,7 +9,7 @@ VALUES
    '58f9dfa7-ecc1-4da9-8946-12872b6366db',
    'Noob team'),
 
-  ('c37ffeb3-6a9e-4d94-8b8e-fbb2c08b73d9',
+  ('b3dbd44c-eed2-4376-9c88-97c08b73a4e3',
    '58f9dfa7-ecc1-4da9-8946-12872b6366db',
    'Code ramparts')
 ;
