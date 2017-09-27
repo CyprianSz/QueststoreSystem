cf7379fea694e47880b8d67367450bdca2fb9500
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 21:44:02 +0200

    Create migration file with artifact data insertion

---
+++ src/main/resources/db/migration/V24__Insert_artifacts_data.sql
@@ -0,0 +1,51 @@
+INSERT INTO artifacts
+(id, name, description, type, value)
+VALUES
+  ('72268d14-0f10-4c2a-a746-3d32adcd2df8',
+   'Combat training',
+   'Private mentoring',
+   'single',
+   '50'),
+
+  ('bf6f99b9-4b2c-41ea-98a3-b67848c05941',
+   'Sanctuary',
+   'You can spend a day in home office',
+   'single',
+   '1000'),
+
+  ('cab694a5-3998-4978-be2d-082cd2cad98f',
+   'Time Travel',
+   'extend SI week assignment deadline by one day',
+   'single',
+   '750'),
+
+  ('9b6984a8-8006-43d0-99cb-b0a94142e52b',
+   'Circle of Sorcery',
+   '60 min workshop by a mentor(s) of the chosen topic',
+   'group',
+   '6000'),
+
+  ('36ff7660-a819-425c-b7a7-ffbebe39b071',
+   'Summon Code Elemental',
+   'mentor joins a students team for a one hour',
+   'group',
+   '2500'),
+
+  ('80cb6ee5-8461-4eb8-8f3b-fba4d42ad9e8',
+   'Tome of knowledge',
+   'Extra material for the current topic',
+   'group',
+   '1500'),
+
+  ('a69349b7-3f41-4340-96c7-dc5d6d267bcf',
+   'Transform mentors',
+   'All mentors should dress up as pirates (or just funny) for the day',
+   'group',
+   '5000'),
+
+  ('0eb8f5aa-c1f0-4a43-a340-428447c3ba42',
+   'Teleport',
+   'The whole course goes to an off-school program instead for a day',
+   'group',
+   '30000')
+;
