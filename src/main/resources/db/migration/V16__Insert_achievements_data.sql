01db0811fb137491a5d0858a405fa266b32db84d
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 18:22:14 +0200

    Change wrong spelling

--- src/main/resources/db/migration/V16__Insert_achievements_data.sql
+++ src/main/resources/db/migration/V16__Insert_achievements_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO codecoolers
+INSERT INTO achievements
 (id, quest_id, codecooler_id, creation_date)
 VALUES
   ('0a4161e6-b117-4ab3-97ab-4d2dddeda196',
@@ -6,18 +6,18 @@ VALUES
    'f1032055-14dd-48a6-9280-a8b3d54b5bb4',
    '2017-08-01'),
 
-  ('0a4161e6-b117-4ab3-97ab-4d2dddeda196',
+  ('b22f9eb7-67ee-4c9c-a2af-36ac9891dd60',
    '5dd16490-2c58-4200-859c-d13b5d12b4b4',
    'a9cf8afb-8c0c-4674-96da-1497f5e4d87b',
    '2017-08-21'),
 
-  ('0a4161e6-b117-4ab3-97ab-4d2dddeda196',
+  ('87220484-c569-4f9e-8393-4cdde74cd6d1',
    '5dd16490-2c58-4200-859c-d13b5d12b4b4',
    'f1032055-14dd-48a6-9280-a8b3d54b5bb4',
    '2017-05-07'),
 
-  ('0a4161e6-b117-4ab3-97ab-4d2dddeda196',
+  ('5ed9f16f-cc7b-4228-8695-16e3f2dc042d',
    '6feb16b0-b6f6-4d3f-8818-c96f1d8a3484',
    'f1032055-14dd-48a6-9280-a8b3d54b5bb4',
-   '2017-07-15'),
+   '2017-07-15')
 ;
