663646625cf54bff523ff24f5cc5977805bc58ed
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 17:09:56 +0200

    Insert admin data

---
+++ src/main/resources/db/migration/V6__Insert_admins_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO codecoolers
+(id, first_name, last_name, date_of_birth, email, password)
+VALUES
+  ('f609bfcc-e1b6-4b45-a32b-d3df67daa8c1',
+   'Mateusz',
+   'Steliga',
+   '1901-10-20',
+   'mateusz.steliga@cc.com',
+   'qwerty'),
+
+  ('cde35a13-97f0-45c8-ac71-8030bf35f0ac',
+   'Mateusz',
+   'Ostafil',
+   '1907-02-17',
+   'mateusz.ostafil@cc.com',
+   'qwerty')
+;
