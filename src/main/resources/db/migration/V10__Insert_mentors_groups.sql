42b43495f696100bbac56770fd8bb6dc27699136
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 18:21:29 +0200

    Change wrong spelling

--- src/main/resources/db/migration/V10__Insert_mentors_groups.sql
+++ src/main/resources/db/migration/V10__Insert_mentors_groups.sql
@@ -1,4 +1,4 @@
-INSERT INTO mentors_groups ()
+INSERT INTO mentors_groups
 (group_id, mentor_id)
 VALUES
   ('85bdd453-6f22-4d75-a2ca-95cfb1326e74',
@@ -8,5 +8,5 @@ VALUES
    'cde35a13-97f0-45c8-ac71-8030bf35f0ac'),
 
   ('58f9dfa7-ecc1-4da9-8946-12872b6366db',
-   'f609bfcc-e1b6-4b45-a32b-d3df67daa8c1'),
+   'f609bfcc-e1b6-4b45-a32b-d3df67daa8c1')
 ;
