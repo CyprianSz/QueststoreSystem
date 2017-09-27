95bb40b6602a0da0e32e3a52b89fc4ccc0fd2dc0
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 21:21:11 +0200

    Create migration file with wallets data insertion

---
+++ src/main/resources/db/migration/V20__Insert_wallets_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO wallets
+(id, balance, earned_coins)
+VALUES
+  ('6ac26c59-5525-49d6-ab18-37e42cc39701',
+   5000,
+   0),
+
+  ('2185728d-f192-43e8-b177-572117ab843f',
+   1000,
+   0)
+;
