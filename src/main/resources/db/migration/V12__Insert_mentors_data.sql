021f067b809eb961802b392b3f72d31f8e8606a9
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 17:47:39 +0200

    Remove group_id attribute

--- src/main/resources/db/migration/V12__Insert_mentors_data.sql
+++ src/main/resources/db/migration/V12__Insert_mentors_data.sql
@@ -6,14 +6,12 @@ VALUES
    'Steliga',
    '1901-10-20',
    'mateusz.steliga@cc.com',
-   'qwerty',
-   ''),
+   'qwerty'),
 
   ('cde35a13-97f0-45c8-ac71-8030bf35f0ac',
    'Mateusz',
    'Ostafil',
    '1907-02-17',
    'mateusz.ostafil@cc.com',
-   'qwerty',
-   '')
+   'qwerty')
 ;
