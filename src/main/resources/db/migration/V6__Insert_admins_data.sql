9d5e975e85efb4c19fe86d855d967b0329ca72a7
Iwo <iwo.tyszkowski@gmail.com>
2017-09-27 17:36:10 +0200

    Modify admins data

--- src/main/resources/db/migration/V6__Insert_admins_data.sql
+++ src/main/resources/db/migration/V6__Insert_admins_data.sql
@@ -1,17 +1,10 @@
 INSERT INTO codecoolers
 (id, first_name, last_name, date_of_birth, email, password)
 VALUES
-  ('f609bfcc-e1b6-4b45-a32b-d3df67daa8c1',
-   'Mateusz',
-   'Steliga',
-   '1901-10-20',
-   'mateusz.steliga@cc.com',
-   'qwerty'),
-
-  ('cde35a13-97f0-45c8-ac71-8030bf35f0ac',
-   'Mateusz',
-   'Ostafil',
-   '1907-02-17',
-   'mateusz.ostafil@cc.com',
-   'qwerty')
+  ('4ab8c123-1fc4-4678-a275-c1fdd35a2deb',
+   'Drop',
+   'Table',
+   '1912-01-01',
+   'drop.table@cc.com',
+   'adminchuj')
 ;
