10a0f929951247190232dc63bc57bc2b851c2607
CyprianS <szwajka.cyprian@gmail.com>
2017-09-28 10:51:00 +0200

    Imlement GroupDAO

---
+++ src/main/java/pl/coderampart/DAO/GroupDAO.java
@@ -0,0 +1,87 @@
+package pl.coderampart.DAO;
+
+import pl.coderampart.model.Group;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class GroupDAO extends AbstractDAO {
+
+    public ArrayList<Group> readAll() {
+        ArrayList<Group> groupList = new ArrayList<>();
+
+        try {
+            Connection connection = this.connectToDataBase();
+            String query = "SELECT * FROM groups;";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Group group = this.createGroupFromResultSet(resultSet);
+                groupList.add(group);
+            }
+            connection.close();
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+
+        return groupList;
+    }
+
+    public void create(Group group) {
+        try {
+            Connection connection = this.connectToDataBase();
+            String query = "INSERT INTO groups VALUES (?, ?);";
+            PreparedStatement statement = connection.prepareStatement(query);
+            PreparedStatement setStatement = setPreparedStatement(statement, group);
+            statement.executeUpdate();
+
+            connection.close();
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+
+    public void update(Group group) {
+        try {
+            Connection connection = this.connectToDataBase();
+            String query = "UPDATE groups SET name = ?, type = ?, value = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            PreparedStatement setStatement = setPreparedStatement(statement, group);
+            setStatement.executeUpdate();
+
+            connection.close();
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+
+    public void delete(Group group) {
+        try {
+            Connection connection = this.connectToDataBase();
+            String query = "DELETE FROM groups WHERE ?;";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, group.getID());
+            statement.executeUpdate();
+
+            connection.close();
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+
+    private PreparedStatement setPreparedStatement(PreparedStatement statement, Group group) throws Exception {
+        statement.setString(1, group.getID());
+        statement.setString(2, group.getName());
+
+        return statement;
+    }
+
+    private Group createGroupFromResultSet(ResultSet resultSet) throws Exception {
+        String ID = resultSet.getString("id");
+        String name = resultSet.getString("name");
+
+        return new Group(ID, name);
+    }
+}
