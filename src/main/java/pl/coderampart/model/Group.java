28b2723f649525bbc4d233be9bbd80261e2a55e9
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-09-28 21:12:03 +0200

    add setName

--- src/main/java/pl/coderampart/model/Group.java
+++ src/main/java/pl/coderampart/model/Group.java
@@ -27,4 +27,5 @@ public class Group{
 
     public String getID() { return this.ID; }
     public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
 }
