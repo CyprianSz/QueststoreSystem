16b3b3ca4ebd0ad4e3fa10d084b4ad4e72f76462
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-09-28 11:29:05 +0200

    Display all mentors details (displayMentors)

--- src/main/java/pl/coderampart/controller/AdminController.java
+++ src/main/java/pl/coderampart/controller/AdminController.java
@@ -1,17 +1,18 @@
 package pl.coderampart.controller;
 
+import pl.coderampart.DAO.MentorDAO;
 import pl.coderampart.model.Admin;
 import pl.coderampart.services.Bootable;
 import pl.coderampart.view.View;
 import pl.coderampart.model.Mentor;
 import java.util.ArrayList;
 import java.util.Arrays;
-// import pl.coderampart.DAO.MentorDAO;
 // import pl.coderampart.DAO.AdminDAO;
 
 public class AdminController implements Bootable {
 
     private View view = new View();
+    private MentorDAO mentorDao = new MentorDAO();
 
     public static final int CREATE_MENTOR = 1;
     public static final int CREATE_GROUP = 2;
@@ -53,44 +54,23 @@ public class AdminController implements Bootable {
 
         Mentor newMentor = new Mentor(mentorData[0], mentorData[1], mentorData[2], mentorData[3],
                                       view.stringToDate(mentorData[4]));
+
+        mentorDao.create(newMentor);
     }
 
     public void editMentor(){
 
-        String mentor
-        final int EDIT_FIRSTNAME = 1;
-        final int EDIT_LASTNAME = 2;
-        final int EDIT_EMAIL = 3;
-        final int EDIT_PASSWORD = 4;
-        final int EDIT_BIRTHDATE = 5;
-
-        ArrayList<String> mentorOptions = new ArrayList<>(Arrays.asList("Edit first name.", "Edit last name.",
-                                                                        "Edit email address.", "Edit password.",
-                                                                        "Edit birth date."));
-
-        view.displayOptions(mentorOptions);
-
-        int editChoice = view.getUserChoice();
-
-        view.clearTerminal();
-
-        switch(editChoice) {
-
-            case EDIT_FIRSTNAME:
-                break;
-            case EDIT_LASTNAME:
-                break;
-            case EDIT_EMAIL:
-                break;
-            case EDIT_PASSWORD:
-                break;
-            case EDIT_BIRTHDATE:
-                break;
-        }
     }
 
     public void displayMentors(){
+        ArrayList<Mentor> mentorList = mentorDao.readAll();
+        ArrayList<String> mentorStrings = new ArrayList<String>();
 
+        for (Mentor mentor: mentorList){
+            mentorStrings.add(mentor.toString());
+        }
+
+        view.outputTable(mentorStrings);
     }
 
     public void createGroup(){
