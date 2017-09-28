718ee35850d63a57494cde3cd9f01d289745b84c
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-09-28 13:56:33 +0200

    Group management - create new group, display all groups, assign new mentor to a group

--- src/main/java/pl/coderampart/controller/AdminController.java
+++ src/main/java/pl/coderampart/controller/AdminController.java
@@ -1,24 +1,25 @@
 package pl.coderampart.controller;
 
 import pl.coderampart.DAO.MentorDAO;
-import pl.coderampart.model.Admin;
+import pl.coderampart.DAO.GroupDAO;
 import pl.coderampart.services.Bootable;
 import pl.coderampart.view.View;
 import pl.coderampart.model.Mentor;
+import pl.coderampart.model.Group;
 import java.util.ArrayList;
-import java.util.Arrays;
-// import pl.coderampart.DAO.AdminDAO;
 
 public class AdminController implements Bootable {
 
     private View view = new View();
     private MentorDAO mentorDao = new MentorDAO();
+    private GroupDAO groupDao = new GroupDAO();
 
     public static final int CREATE_MENTOR = 1;
     public static final int CREATE_GROUP = 2;
     public static final int EDIT_MENTOR = 3;
     public static final int DISPLAY_MENTOR = 4;
-    public static final int CREATE_LEVEL = 5;
+    public static final int DISPLAY_GROUP = 5;
+    public static final int CREATE_LEVEL = 6;
     public static final int EXIT = 0;
 
     public boolean start() {
@@ -36,6 +37,8 @@ public class AdminController implements Bootable {
             case EDIT_MENTOR: editMentor();
                 break;
             case DISPLAY_MENTOR: displayMentors();
+                break;
+            case DISPLAY_GROUP: displayGroups();
                 break;
             case CREATE_LEVEL: createLevel();
                 break;
@@ -55,14 +58,22 @@ public class AdminController implements Bootable {
         Mentor newMentor = new Mentor(mentorData[0], mentorData[1], mentorData[2], mentorData[3],
                                       view.stringToDate(mentorData[4]));
 
+        this.displayGroups();
+        ArrayList<Group> allGroups = groupDao.readAll();
+        String chosenGroupName = view.getInput("Enter name of a group you wish to assign this mentor to: ");
+        for (Group group: allGroups){
+            if (chosenGroupName.equals(group.getName())){
+                newMentor.setGroup(group);
+            }
+        }
         mentorDao.create(newMentor);
     }
 
     public void editMentor(){
         this.displayMentors();
-        ArrayList<Mentor> mentorList = mentorDao.readAll();
+        ArrayList<Mentor> allMentors = mentorDao.readAll();
         String chosenMentorEmail = view.getInput("Enter email of a mentor you wish to edit:");
-        for (Mentor mentor: mentorList){
+        for (Mentor mentor: allMentors){
             if (chosenMentorEmail.equals(mentor.getEmail())){
                 mentorDao.update(mentor);
             }
@@ -70,18 +81,33 @@ public class AdminController implements Bootable {
     }
 
     public void displayMentors(){
-        ArrayList<Mentor> mentorList = mentorDao.readAll();
+        ArrayList<Mentor> allMentors = mentorDao.readAll();
         ArrayList<String> mentorStrings = new ArrayList<String>();
 
-        for (Mentor mentor: mentorList){
+        for (Mentor mentor: allMentors){
             mentorStrings.add(mentor.toString());
         }
 
         view.outputTable(mentorStrings);
     }
 
-    public void createGroup(){
+    public void displayGroups(){
+        ArrayList<Group> allGroups = groupDao.readAll();
+        ArrayList<String> groupStrings = new ArrayList<String>();
 
+        for (Group group: allGroups){
+            groupStrings.add(group.toString());
+        }
+
+        view.outputTable(groupStrings);
+    }
+
+    public void createGroup(){
+        String[] groupData = view.getGroupData();
+
+        Group newGroup = new Group(groupData[0]);
+
+        groupDao.create(newGroup);
     }
 
     public void createLevel(){
