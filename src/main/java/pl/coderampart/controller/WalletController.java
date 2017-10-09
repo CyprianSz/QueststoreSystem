c8c1c70659eecaff681db5211ae04a926ffff1fc
CyprianS <szwajka.cyprian@gmail.com>
2017-10-09 11:04:29 +0200

    Implement changeBalance to WalletController

--- src/main/java/pl/coderampart/controller/WalletController.java
+++ src/main/java/pl/coderampart/controller/WalletController.java
@@ -1,5 +1,23 @@
 package pl.coderampart.controller;
 
+import pl.coderampart.DAO.WalletDAO;
+import pl.coderampart.model.Codecooler;
+import pl.coderampart.model.Wallet;
+
 public class WalletController {
 
+    private WalletDAO walletDAO = new WalletDAO();
+
+    public void changeBalance(Codecooler codecooler, Integer coins) {
+        Integer balance = codecooler.getWallet().getBalance();
+        Integer earnedCoins = codecooler.getWallet().getEarnedCoins();
+        balance += coins;
+        earnedCoins += coins;
+
+        Wallet wallet = codecooler.getWallet();
+        wallet.setBalance(balance);
+        wallet.setEarnedCoins(earnedCoins);
+
+        walletDAO.update(wallet);
+    }
 }
