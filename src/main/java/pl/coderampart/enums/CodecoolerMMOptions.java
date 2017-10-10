9a47aea0b2250660db64f3e0cf9a0d32eba24fea
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-10-10 11:36:20 +0200

    add enum to store Codecooler Management Menu options

---
+++ src/main/java/pl/coderampart/enums/CodecoolerMMOptions.java
@@ -0,0 +1,11 @@
+package pl.coderampart.enums;
+
+public enum CodecoolerMMOptions {
+    BACK_TO_MAIN_MENU,
+    CREATE_CODECOOLER,
+    EDIT_CODECOOLER,
+    DISPLAY_CODECOOLERS,
+    CREATE_ACHIEVEMENT,
+    MARK_ITEM,
+    DISPLAY_WALLET
+}
