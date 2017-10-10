1d5fd6d79170dac14982676d00ee3b975f7840fe
Iwo <iwo.tyszkowski@gmail.com>
2017-10-10 11:32:51 +0200

    Change admin data for easier testing

--- src/main/resources/db/migration/V6__Insert_admins_data.sql
+++ src/main/resources/db/migration/V6__Insert_admins_data.sql
@@ -2,9 +2,9 @@ INSERT INTO admins
 (id, first_name, last_name, date_of_birth, email, password)
 VALUES
   ('4ab8c123-1fc4-4678-a275-c1fdd35a2deb',
-   'Drop',
-   'Table',
+   'Admin',
+   'Adminski',
    '1912-01-01',
-   'drop.table@cc.com',
-   'adminchuj')
+   'admin@cc.com',
+   'admin123')
 ;
