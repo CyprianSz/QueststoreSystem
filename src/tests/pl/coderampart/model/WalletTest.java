379a03dac578143a1f845cad7508289eab915d75
Ragnarockie08 <Ragnarockie08@gmail.com>
2017-10-12 12:42:57 +0200

    tests in wallet test class created

--- src/tests/pl/coderampart/model/WalletTest.java
+++ src/tests/pl/coderampart/model/WalletTest.java
@@ -1,10 +1,19 @@
 package pl.coderampart.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class WalletTest {
+
+    private Wallet wallet;
+
+    @BeforeEach
+    void setup(){
+        this.wallet = new Wallet("id", 100, 100);
+    }
 
     @Test
     void test1WalletConstructor(){
@@ -14,8 +23,34 @@ class WalletTest {
 
     @Test
     void test2WalletConstructor(){
-        Wallet wallet = new Wallet("id", 5, 5);
         assertNotNull(wallet);
+    }
+
+    @Test
+    void testWalletGetId(){
+        assertEquals("id", wallet.getID());
+    }
+
+    @Test
+    void testWalletGetBallance(){
+        assertEquals(Integer.valueOf(100), wallet.getBalance());
+    }
+
+    @Test
+    void testWalletGetEarned(){
+        assertEquals(Integer.valueOf(100), wallet.getEarnedCoins());
+    }
+
+    @Test
+    void testWalletSetBalance(){
+        wallet.setBalance(110);
+        assertEquals(Integer.valueOf(110), wallet.getBalance());
+    }
+
+    @Test
+    void testWalletSetEarnedCoins(){
+        wallet.setEarnedCoins(110);
+        assertEquals(Integer.valueOf(110), wallet.getEarnedCoins());
     }
 
 }
