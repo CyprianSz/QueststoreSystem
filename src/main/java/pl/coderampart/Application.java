4aacd112d923678f07503f4e4ea7608333b92c46
natahrytsyna <natalya.hrytsyna@gmail.com>
2017-11-07 17:42:43 +0100

    Add static class and set server in Application

--- src/main/java/pl/coderampart/Application.java
+++ src/main/java/pl/coderampart/Application.java
@@ -1,15 +1,29 @@
 package pl.coderampart;
 
+import com.sun.net.httpserver.HttpServer;
+import pl.coderampart.controller.admin.CreateMentorController;
+import pl.coderampart.controller.helpers.Static;
+
+import java.net.InetSocketAddress;
 import java.sql.SQLException;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
-        try {
-            Logger logger = new Logger();
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+        server.createContext("/create-mentor", new CreateMentorController());
+        server.createContext("/static", new Static());
+        server.setExecutor(null);
+
+        server.start();
     }
+//    public static void main(String[] args) {
+//
+//        try {
+//            Logger logger = new Logger();
+//        } catch (SQLException e){
+//            e.printStackTrace();
+//        }
+//    }
 }
