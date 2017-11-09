5bde33e5e85a71aabee91c58143f291d5befe5ca
Iwo <iwo.tyszkowski@gmail.com>
2017-11-09 17:03:15 +0100

    Change method reference for common class reference

--- src/main/java/pl/coderampart/controller/admin/DeleteMentorController.java
+++ src/main/java/pl/coderampart/controller/admin/DeleteMentorController.java
@@ -5,6 +5,7 @@ import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 import pl.coderampart.DAO.MentorDAO;
+import pl.coderampart.controller.helpers.HelperController;
 import pl.coderampart.model.Mentor;
 
 import java.io.*;
@@ -15,100 +16,91 @@ import java.util.*;
 
 public class DeleteMentorController implements HttpHandler {
 
-        private Connection connection;
-        private MentorDAO mentorDAO;
+    private Connection connection;
+    private MentorDAO mentorDAO;
+    private HelperController helperController;
 
-        public DeleteMentorController(Connection connection) {
-            this.connection = connection;
-            this.mentorDAO = new MentorDAO(this.connection);
+    public DeleteMentorController(Connection connection) {
+        this.connection = connection;
+        this.mentorDAO = new MentorDAO(this.connection);
+        this.helperController = new HelperController();
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String method = httpExchange.getRequestMethod();
+        String response = "";
+
+        List<Mentor> allMentors = readMentorsFromDB();
+
+        String[] uri = httpExchange.getRequestURI().toString().split("=%2F");
+        String id = uri[uri.length-1];
+
+        response += helperController.renderHeader(httpExchange);
+        response += helperController.render("admin/adminMenu");
+        response += renderMentorsList(allMentors);
+        response += helperController.render("footer");
+
+        if(method.equals("POST")) {
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String formData = br.readLine();
+            Map inputs = parseFormData(formData);
+            if (inputs.get("confirmation").equals("yes")) {
+                deleteMentor(allMentors, id);
+            }
         }
 
-        @Override
-        public void handle(HttpExchange httpExchange) throws IOException {
-            String method = httpExchange.getRequestMethod();
-            String response = "";
+        httpExchange.sendResponseHeaders( 200, response.getBytes().length );
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 
-            List<Mentor> allMentors = readMentorsFromDB();
+    private List<Mentor> readMentorsFromDB() {
+        List<Mentor> allMentors = null;
 
-            String[] uri = httpExchange.getRequestURI().toString().split("=%2F");
-            String id = uri[uri.length-1];
+        try {
+            allMentors = mentorDAO.readAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allMentors;
+    }
 
-            response += render("header");
-            response += render("admin/adminMenu");
-            response += renderMentorsList(allMentors);
-            response += render("footer");
+    private String renderMentorsList(List<Mentor> allMentors) {
+        String templatePath = "templates/admin/deleteMentor.twig";
+        JtwigTemplate template = JtwigTemplate.classpathTemplate( templatePath );
+        JtwigModel model = JtwigModel.newModel();
 
+        model.with("allMentors", allMentors);
 
-            if(method.equals("POST")){
+        return template.render(model);
+    }
 
-                InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
-                BufferedReader br = new BufferedReader(isr);
-                String formData = br.readLine();
-                Map inputs = parseFormData(formData);
-                if(inputs.get("confirmation").equals("yes")) {
-                    deleteMentor(allMentors, id);
+    private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            String value = URLDecoder.decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
+    }
 
+    private void deleteMentor(List<Mentor> allMentors,String id) {
+        Mentor changedMentor = null;
+        for (Mentor mentor: allMentors) {
+            if (id.equals(mentor.getID())) {
+                changedMentor = mentor;
+                try {
+                    mentorDAO.delete(changedMentor);
+                } catch (SQLException e) {
+                    e.printStackTrace();
                 }
-            }
-
-            httpExchange.sendResponseHeaders( 200, response.getBytes().length );
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-
-            os.close();
-        }
-
-        private List<Mentor> readMentorsFromDB() {
-            List<Mentor> allMentors = null;
-
-            try {
-                allMentors = mentorDAO.readAll();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return allMentors;
-        }
-
-        private String render(String fileName) {
-            String templatePath = "templates/" + fileName + ".twig";
-            JtwigTemplate template = JtwigTemplate.classpathTemplate( templatePath );
-            JtwigModel model = JtwigModel.newModel();
-
-            return template.render(model);
-        }
-
-        private String renderMentorsList(List<Mentor> allMentors) {
-            String templatePath = "templates/admin/deleteMentor.twig";
-            JtwigTemplate template = JtwigTemplate.classpathTemplate( templatePath );
-            JtwigModel model = JtwigModel.newModel();
-
-            model.with("allMentors", allMentors);
-
-            return template.render(model);
-        }
-
-        private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
-            Map<String, String> map = new HashMap<>();
-            String[] pairs = formData.split("&");
-            for(String pair : pairs){
-                String[] keyValue = pair.split("=");
-                String value = URLDecoder.decode(keyValue[1], "UTF-8");
-                map.put(keyValue[0], value);
-            }
-            return map;
-        }
-
-        private void deleteMentor(List<Mentor> allMentors,String id) {
-            Mentor changedMentor = null;
-            for (Mentor mentor: allMentors) {
-                if (id.equals(mentor.getID())) {
-                    changedMentor = mentor;
-                    try{
-
-                        mentorDAO.delete(changedMentor);
-                    }catch (SQLException se){}
-                    break;
-                }
+                break;
             }
         }
+    }
 }
