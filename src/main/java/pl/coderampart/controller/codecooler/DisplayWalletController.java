1d32c16cdff3fb0a149a840e381cad2344a1b56f
Iwo <iwo.tyszkowski@gmail.com>
2017-11-10 09:56:20 +0100

    Create controller for backend of Codecooler and displayWallet

---
+++ src/main/java/pl/coderampart/controller/codecooler/DisplayWalletController.java
@@ -0,0 +1,34 @@
+package pl.coderampart.controller.codecooler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import pl.coderampart.DAO.WalletDAO;
+import pl.coderampart.controller.helpers.HelperController;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.Connection;
+
+public class DisplayWalletController implements HttpHandler {
+
+    private Connection connection;
+    private WalletDAO walletDAO;
+    private HelperController helperController;
+
+    public DisplayWalletController(Connection connection) {
+        this.connection = connection;
+        this.walletDAO = new WalletDAO(this.connection);
+        this.helperController = new HelperController();
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "";
+        response += helperController.renderHeader(httpExchange);
+        response += helperController.render("codecooler/displayWallet");
+        httpExchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
