258deceb040a11c9b1d26984d045f3c6d7fe5576
Iwo <iwo.tyszkowski@gmail.com>
2017-11-20 13:42:26 +0100

    Create Fundraising model class with proper fields

---
+++ src/main/java/pl/coderampart/model/Fundraising.java
@@ -0,0 +1,12 @@
+package pl.coderampart.model;
+
+import pl.coderampart.controller.UUIDController;
+import java.time.LocalDate;
+
+public class Fundraising {
+
+    private String ID;
+    private String name;
+    private LocalDate creationDate;
+    private boolean isOpen;
+}
