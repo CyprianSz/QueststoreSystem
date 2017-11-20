7fc8de4ff77acd44b8ce0db3516df66dbea83ed1
Iwo <iwo.tyszkowski@gmail.com>
2017-11-20 15:20:26 +0100

    Add creator_id column to fundraisings table

--- src/main/resources/db/migration/V25__Create_fundraisings_table.sql
+++ src/main/resources/db/migration/V25__Create_fundraisings_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE fundraisings (
   id VARCHAR(36),
+  artifact_id VARCHAR(36),
   name VARCHAR(50) NOT NULL,
   creation_date DATE,
   creator_id VARCHAR(50),
   is_open BOOLEAN DEFAULT 1,
   PRIMARY KEY (id),
+  FOREIGN KEY (artifact_id) REFERENCES artifacts(id),
   FOREIGN KEY (creator_id) REFERENCES codecoolers(id)
 );
