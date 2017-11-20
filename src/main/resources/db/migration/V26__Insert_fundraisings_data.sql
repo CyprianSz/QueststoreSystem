cc855dea9c80bffe80698f3933d32b8ca5c32996
Iwo <iwo.tyszkowski@gmail.com>
2017-11-20 15:21:57 +0100

    Add migration with test data insertion for fundraisings

---
+++ src/main/resources/db/migration/V26__Insert_fundraisings_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO fundraisings
+(id, artifact_id, name, creation_date, creator_id, is_open)
+VALUES
+  ('f10ab055-14dd-48a6-9280-a8b3d54b5bb4',
+   '9b6984a8-8006-43d0-99cb-b0a94142e52b',
+   'Ściepa na berło czystości',
+   '2017-11-11',
+   'f1032055-14dd-48a6-9280-a8b3d54b5bb4',
+   '1'),
+
+  ('f10ab015-14dd-48a6-9280-a8b3d54b5bb4',
+   '0eb8f5aa-c1f0-4a43-a340-428447c3ba42',
+   'Zrzuta na teleport',
+   '2017-11-15',
+   'a9cf8afb-8c0c-4674-96da-1497f5e4d87b',
+   '1'),
+
+  ('f10ab055-14dd-48a6-9280-a8bjtp4b5bb4',
+   '36ff7660-a819-425c-b7a7-ffbebe39b071',
+   'Zrzutka na mentora',
+   '2017-10-09',
+   'f1032055-14dd-48a6-9280-a8b3d54b5bb4',
+   '0')
+;
