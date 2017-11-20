b04ef9e89f25a7672e4b9744a02aa00f3a48c730
Iwo <iwo.tyszkowski@gmail.com>
2017-11-20 13:58:25 +0100

    Add fake contructor

--- src/main/java/pl/coderampart/model/Fundraising.java
+++ src/main/java/pl/coderampart/model/Fundraising.java
@@ -8,5 +8,14 @@ public class Fundraising {
     private String ID;
     private String name;
     private LocalDate creationDate;
+    private Codecooler codecooler;
     private boolean isOpen;
+
+    public Fundraising(String name, Codecooler codecooler) {
+        this.ID = UUIDController.createUUID();
+        this.name = name;
+        this.creationDate = LocalDate.now();
+        this.codecooler = codecooler;
+        this.isOpen = true;
+    }
 }
