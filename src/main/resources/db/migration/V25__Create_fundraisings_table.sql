d8e487bdce963ee729d220cc9af67487ed6aa45b
Iwo <iwo.tyszkowski@gmail.com>
2017-11-20 14:46:47 +0100

    Add migration for create fundraisings table

--- src/main/resources/db/migration/V25__Create_fundraisings_table.sql
+++ src/main/resources/db/migration/V25__Create_fundraisings_table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE fundraisings (
   id VARCHAR(36),
-  name TEXT UNIQUE,
+  name VARCHAR(50) NOT NULL,
   creation_date DATE,
+  creator_id VARCHAR(50),
   is_open BOOLEAN DEFAULT 1,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (creator_id) REFERENCES codecoolers(id)
 );
