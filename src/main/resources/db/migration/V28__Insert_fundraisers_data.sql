8985c6467ba89bf3f4240428852b2a681cf7a14e
Iwo <iwo.tyszkowski@gmail.com>
2017-11-20 16:01:08 +0100

    Add insertion migration for fundraisers table

---
+++ src/main/resources/db/migration/V28__Insert_fundraisers_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO fundraisers
+(fundraiser_id, fundraising_id)
+VALUES
+  ('f1032055-14dd-48a6-9280-a8b3d54b5bb4',
+   'f10ab055-14dd-48a6-9280-a8b3d54b5bb4'),
+
+  ('f1032055-14dd-48a6-9280-a8b3d54b5bb4',
+   'f10ab015-14dd-48a6-9280-aabcd54b5bb4'),
+
+  ('a9cf8afb-8c0c-4674-96da-1497f5e4d87b',
+   'f10ab055-14dd-48a6-9280-a8b3d54b5bb4'),
+
+  ('a9cf8afb-8c0c-4674-96da-1497f5e4d87b',
+   'f10ab015-14dd-48a6-9280-aabcd54b5bb4'),
+
+  ('f1032055-14dd-48a6-9280-a8b3d54b5bb4',
+   'f10ab055-14dd-48a6-9280-a8bjtp4b5bb4')
+;
