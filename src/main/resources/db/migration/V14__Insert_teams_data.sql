f158355c34d4464d2ca5a5155a65fe73e6e25ca2
natahrytsyna <natalya.hrytsyna@gmail.com>
2017-11-30 17:31:43 +0100

    Add migrations for teams from different classes, create codecoolers from other groups

--- src/main/resources/db/migration/V14__Insert_teams_data.sql
+++ src/main/resources/db/migration/V14__Insert_teams_data.sql
@@ -14,14 +14,22 @@ VALUES
    'Code-rampart'),
 
   ('b3dbd44c-eed2-4376-9c88-47c18b03a6e7',
-  '85bdd453-6f22-4d75-a2ca-95cfb1326e74',
-  'MMBK'),
+   '85bdd453-6f22-4d75-a2ca-95cfb1326e74',
+   'MMBK'),
 
   ('ca6dbab9-dc44-41a8-8dc2-6e80797cce82',
-  '85bdd453-6f22-4d75-a2ca-95cfb1326e74',
-  'MJS'),
+   '85bdd453-6f22-4d75-a2ca-95cfb1326e74',
+   'MJS'),
 
   ('9976b54e-86ee-47aa-9b23-ec60feab314c',
-  '85bdd453-6f22-4d75-a2ca-95cfb1326e74',
-  'BPT')
+   '85bdd453-6f22-4d75-a2ca-95cfb1326e74',
+   'BPT'),
+
+  ('dce09d6e-45c6-4dc9-80c2-b106c990dcd8',
+   '58f9dfa7-ecc1-4da9-8946-12872b6366db',
+   'Koderzy'),
+
+  ('3difocp3-6afd-469f-a850-6d8d24957a3a',
+   '78f9gha7-ecc1-4da9-8946-12872b6366db',
+   'Codecoolerzy')
 ;
